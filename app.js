709e5c8afbf90798a0962432e047a2628ea8146a
MahnoorHaider <125372033+mahnoorhyder-bit@users.noreply.github.com>
2023-04-07 20:24:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+// // question-1
+// let num1 = prompt("Enter the first number:");
+// let num2 = prompt("Enter the second number:");
+
+// // Convert input strings to numbers
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// if (num1 > num2) {
+//   console.log(num1 + " is larger than " + num2);
+// } else if (num2 > num1) {
+//   console.log(num2 + " is larger than " + num1);
+// } else {
+//   console.log("The two numbers are equal.");
+// }
+// // Question-2
+// let num = prompt("Enter a number:");
+// let sign;
+
+// if (num > 0) {
+//   sign = "+";
+// } else if (num < 0) {
+//   sign = "-";
+// } else {
+//   sign = "0";
+// }
+// alert("The sign is " + sign);
+//question-3
+// let num1= prompt("Enter number");
+// let num2= prompt("Enter number");
+// let num3= prompt("Enter number");
+// let num4= prompt("Enter number");
+// let num5= prompt("Enter number");
+//  let greaterNumber= Math.max(num1, num2 ,num3, num4, num5);
+//     console.log("num1 is greater"+ greaterNumber);
+    
+// question-4
+// for (let i=0 ; i<=15 ; i++){
+//     if (i % 2 === 0){
+//         console.log(i + " is even");
+//     } else{
+//         console.log(i + " is odd");
+//     }
+// }
+// question-5
+// const marks = [80 , 90 , 70, 60, 85];
+// let sum = 0;
+//  for (let i= 0; i< marks.length; i++ ){
+//     sum+= marks[i];
+//  } 
+//  const avg = sum/ marks.length;
+//  let grade;
+//  if (avg < 60){
+//     grade = "F";
+//  }
+//  else if (avg < 70){
+//     grade = "D";
+//  }
+//  else if (avg < 80){
+//     grade = "C";
+//  }
+//  else if (avg < 90){
+//     grade = "B";
+//  }
+//  else {
+//     grade = "A";
+//  }
+//  console.log("Average marks : " + avg);
+// console.log("Grade: " + grade );
+
+// // question-6
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// //  question-7
+// for (var i=1 ; i<=5 ; i++){
+//       var line= '';
+//     for (var j=1 ; j<=i ; j++){
+//     line +="*";
+// }
+// console.log(line);
+// }
